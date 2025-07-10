6cdf338c7129cb8835388aa7ea915f6f61db3d5e
Thirdbase1 <miraclethirdbase1@gmail.com>
2025-07-10 23:59:37 +0100

    Relaxed email validation

--- utils/validator.js
+++ utils/validator.js
@@ -1,4 +1,4 @@
-export default function validateEmail(email) {
-  const regex = /^[\\w.-]+@[\\w.-]+\\.\\w{2,}$/;
-  return regex.test(email);
+export function isValidEmail(email) {
+  const basicRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return basicRegex.test(email);
 }
